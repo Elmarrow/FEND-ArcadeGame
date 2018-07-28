f1837f4bf5892588fe444fbcfb2050e221cabfb9
Aggelos Afratis <aafratis@gmail.com>
2018-07-28 22:58:32 +0300

    Player method, Initiation method, checking collision method

--- js/app.js
+++ js/app.js
@@ -37,20 +37,54 @@ const Player = function (x, y){
     this.x = x;
     this.y=y;
     this.sprite = 'images/char-horn-girl.png';
-}
+};
 
 Player.prototype.update = function(){
     playerX = this.x;
     playerY = this.y;
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Player.prototype.reset = function(){
+    this.x = 200;
+    this.y = 380;
+}
+
+Player.prototype.handleInput = function(pressedKeys){
+    if (pressedKeys === 'up'&& this.y > 18){
+        this.y -= 80;
+    }
+    else if (ppressedKeys === "right" && this.x < 400){
+        this.x += 100;
+    }
+    else if (pressedKeys === 'down' && this.y < 380){
+        this.y += 80;
+    }
+    else if (pressedKeys === 'left' && this.x > 33){
+        this.x -= 100;
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 
+var checkCollisions = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (player.x < allEnemies[i].x + enemyWidth &&
+            player.x + playerWidth > allEnemies[i].x &&
+            player.y < allEnemies[i].y + enemyHeight &&
+            player.y + playerHeight > allEnemies[i].y
+        ) {
+            player.reset();
+        }
+
+    }
+};
 
 
 // This listens for key presses and sends the keys to your
