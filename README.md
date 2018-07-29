a76b67946da4333bccd0c0ab2fc0cc6a12357ed7
Aggelos Afratis <aafratis@gmail.com>
2018-07-30 01:26:31 +0300

    Project completed

--- README.md
+++ README.md
@@ -1,47 +1,17 @@
 # Classic Arcade Game Clone Project
 
-## Project Instructions from Udacity
+## How to Play
+### Overview
+The point of the game is survival. You start from the safety of the green area. Your goal is to reach the water by crossing the road. ou overGiant bugs running at different speeds won't stop at nothing and will run you over. If you get hit  by the bugs three times and the game is over. If you cross safely to the water you gain 500 points. How many points can you gather before losing all your lives?
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+<br>
 
-## Steps to Complete the Project
 
-* Download the [art assets and provided game engine](https://github.com/udacity/frontend-nanodegree-arcade-game).
-* Read the project instructions above.
-* Learn and review Object Oriented JavaScript & HTML5 Canvas.
-* Understand the [Project Rubric](https://review.udacity.com/?_ga=1.242571394.1230547285.1451946706#!/rubrics/15/view)
-* Follow instructions and comments provided in `app.js` file.
-  - Identify the needed classes: Enemy & Player
-  - Identify and code the properties for each classes
-  - Write functions for each class instances
-* Check using the Project Rubric and debug the project
-* Update the Readme file
+### Moving
 
-## Learning Resources
+Device | UP   | DOWN | LEFT | RIGHT
+----   | ---- | ---- | ---- | ----
+Keyboard | ⬆️ | ⬇️  | ⬅️ | ➡️ |
+Keyboard | w  | s  | a | d 
 
-* [Object Oriented JavaScript](https://classroom.udacity.com/courses/ud015)
-* [HTML5 Canvas](https://www.udacity.com/course/html5-canvas--ud292)
-* [Stackoverflow](https://stackoverflow.com/)
-* Slack
-
-## Game Mechanics
-
-* This game has one player and the goal is to reach the water.
-* Player has to cross the rows of stone blocks without colliding with the enemy which are the bugs.
-* Keyboard arrows (up, down, left & right) are used to move the player.
-* Player has only 3 lives and loses one after a collision.
-* When the player reaches the water, it earns 500 points and goes back to the bottom to cross again.
-
-![Screenshot](https://github.com/888Red/Game---Arcade/blob/master/images/screenshot.jpg)
-
-## Demo
-
-[Let's play!](https://888red.github.io/Game---Arcade/)
-
-## Functionalities to be added in the future
-
-* Add player characters
-* Include gems to be collected by the player to get bonus points
-* Include game obstacles
-* Explore more on HTML5 canvas to draw some texts
-* Add a modal when game is over
+<br>
--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+/*.....................................
+               Enemies                 
+.....................................*/
+
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
@@ -8,73 +12,48 @@ var Enemy = function(x, y, speed) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = "images/enemy-bug.png";
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
 
-    // When enemy is beyond canvas width, it goes back to the left side
+    // Enemy sprite canvas limit checking and initialization with random speed
+    const speedArray = [50, 100, 250, 400];
     if (this.x > 510) {
         this.x = -100;
-        this.speed = 80 + Math.floor(Math.random() * 180);
+        this.speed = speedArray[Math.floor(Math.random() * speedArray.length)];
     }
-    checkCollisions(this);
-};
-
-// When collision happens, player goes back to the bottom,
-// and player's life is reduced
-let checkCollisions = function () {
-   for (var i = 0; i < allEnemies.length; i++) {
-     if (player.x < allEnemies[i].x + 68 &&
-        player.x + 38 > allEnemies[i].x &&
-        player.y < allEnemies[i].y + 38 &&
-        30 + player.y > allEnemies[i].y) {
-          resetPlayer();
-          player.lives -=1;
-          if (player.lives === 0) {
-            gameOver();
-          }
-       }
-    }
-};
-//puts back the player to starting block
-function resetPlayer() {
-    player.x = 210;
-    player.y = 380;
-};
-//alert that game is over (will improve this in the future)
-let gameOver = function() {
-    alert("Sorry! Game Over! Please refresh the page to start again.");
-    counter = 0;
-    showPoints.innerHTML = counter;
-    resetPlayer();
-    player.lives = 0;
+    checkCollision(this);
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/*.....................................
+               Player                  
+.....................................*/
+
 // Now write your own player class
-var Player = function(x, y, speed) {
+var Player = function (x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.sprite = 'images/char-pink-girl.png';
+    this.sprite = 'images/char-horn-girl.png';
     this.lives = 3;
 };
 
 // This class requires an update(), render() and
 // a handleInput() method.
 
-Player.prototype.update = function() {
+Player.prototype.update = function () {
     // Keep player inside the canvas
     if (this.y > 380) {
         this.y = 380;
@@ -97,30 +76,75 @@ Player.prototype.update = function() {
     }
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(keyPress) {
+Player.prototype.handleInput = function (keyPress) {
     switch (keyPress) {
         case 'up':
-          this.y -= this.speed + 30;
-          break;
+            this.y -= this.speed + 30;
+            break;
         case 'down':
-          this.y += this.speed + 30;
-          break;
+            this.y += this.speed + 30;
+            break;
         case 'left':
-          this.x -= this.speed + 50;
-          break;
+            this.x -= this.speed + 50;
+            break;
         case 'right':
-          this.x += this.speed + 50;
-          break;
-     }
+            this.x += this.speed + 50;
+            break;
+    }
+};
+
+/*.....................................
+               Interactions            
+.....................................*/
+
+// When collision happens, player returns at starting point and
+// player looses a life
+let checkCollision = function () {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (player.x < allEnemies[i].x + 68 &&
+            player.x + 38 > allEnemies[i].x &&
+            player.y < allEnemies[i].y + 38 &&
+            30 + player.y > allEnemies[i].y) {
+            resetPlayer();
+            loseLife();
+        }
+    }
+};
+
+//resets the Player to the starting point
+function resetPlayer() {
+    player.x = 210;
+    player.y = 380;
+};
+
+// Loosing life and checking for Game Over
+function loseLife() {
+    // Only decrement when greater than zero
+    if (player.lives > 0) {
+        player.lives--; // decrement lives
+        const lives = document.querySelectorAll('.lifebar img'); // Get the life meter
+        lives[player.lives].classList.toggle('hide'); // Remove one life on screen
+    }
+    if (player.lives === 0) {
+        gameOver();
+    }
+}
+
+//alert that game is over 
+let gameOver = function () {
+    alert("Game Over! Please close the alert to start again.");
+    document.location.reload();
 };
 
 //Show points on the page
 let showPoints = document.getElementById('points');
 let counter = 0;
+
+//Show lives on the page
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -128,18 +152,18 @@ let counter = 0;
 
 var player = new Player(210, 380, 50);
 
-const enemyA = new Enemy(-100, 227.5, 50);
-const enemyB = new Enemy(-500, 227.5, 50);
-const enemyC = new Enemy(-150, 145, 50);
-const enemyD = new Enemy(-400, 61, 50);
-const enemyE = new Enemy(-200, 61, 50);
-const enemyF = new Enemy(-300, 61, 50);
+const bugOne = new Enemy(-100, 229, 50);
+const bugTwo = new Enemy(-500, 229, 50);
+const bugThree = new Enemy(-150, 145, 50);
+const bugFour = new Enemy(-400, 145, 50);
+const bugFive = new Enemy(-200, 61, 50);
+const bugSix = new Enemy(-350, 61, 50);
 let allEnemies = [];
-allEnemies.push(enemyA, enemyB, enemyC, enemyD, enemyE, enemyF);
+allEnemies.push(bugOne, bugTwo, bugThree, bugFour, bugFive, bugSix);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -149,3 +173,11 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function setLives() {
+    const lives = document.querySelectorAll('.lifebar img');
+
+    lives.forEach((life) => {
+        life.src = `./${player.sprite}`;
+    });
+}
